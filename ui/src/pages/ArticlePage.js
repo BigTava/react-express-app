ca456cdda03016da8b83dcef2405fc84ef1b4e99
Tiago Tavares <tiago.tavares@inesctec.pt>
2022-08-06 11:18:19 +0100

    Connects the frontend to backend

--- ui/src/pages/ArticlePage.js
+++ ui/src/pages/ArticlePage.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 import ArticleList from "../components/ArticleList";
 import articleContent from "./article-content";
 import NotFoundPage from "./NotFoundPage";
-import { useParams } from "react-router-dom";
+import CommentsList from "../components/CommentList";
+import UpvotesSection from "../components/upvoteSection";
+import AddCommentForm from "../components/AddCommentForm";
 
 const ArticlePage = () => {
   const { name } = useParams();
   const article = articleContent.find((article) => article.name === name);
+
+  const [articleInfo, setArticleInfo] = React.useState({
+    upvotes: 0,
+    comments: [],
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`/api/articles/${name}`);
+      const body = await result.json();
+      setArticleInfo(body);
+    };
+    fetchData();
+  }, [name]);
 
   if (!article) return <NotFoundPage />;
 
@@ -16,9 +34,16 @@ const ArticlePage = () => {
   return (
     <>
       <h1>{article.title}</h1>
+      <UpvotesSection
+        articleName={name}
+        upvotes={articleInfo.upvotes}
+        setArticleInfo={setArticleInfo}
+      />
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
+      <CommentsList comments={articleInfo.comments} />
+      <AddCommentForm articleName={name} setArticleInfo={setArticleInfo} />
       <h3>Other articles:</h3>
       <ArticleList articles={otherArticles} />
     </>
